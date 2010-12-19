b51a9106595e3067c5c8d3ef0a6f6fdd35843aa0
Breyten Ernsting <bje@dds.nl>
2010-12-19 22:12:22 +0100

    Index logic for tweets.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,30 @@
 class TweetsController < ApplicationController
+  # GET /tweets
+  # GET /tweets.xml
+  def index
+    page_size = 10
+    @tweets = Tweet.where(:deleted => 1)
+    
+    @tweets = @tweets.offset(params[:offset] * page_size) if params.has_key?(:offset)
+    
+    @tweets = @tweets.limit(page_size)
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.xml  { render :xml => @tweets }
+      format.json { render :json => @tweets }
+    end
+  end
+
+  # GET /tweets/1
+  # GET /tweets/1.xml
+  def show
+    @tweet = Tweet.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @tweet }
+      format.json  { render :json => @tweet }
+    end
+  end
 end
