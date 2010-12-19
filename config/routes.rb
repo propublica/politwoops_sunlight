a34103fce2fcc610a2a709f4ead01baa3e4879c8
Breyten Ernsting <bje@dds.nl>
2010-12-19 23:13:39 +0100

    Copy old layout over.

--- config/routes.rb
+++ config/routes.rb
@@ -48,10 +48,12 @@ Politwoops::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "tweets#index"
 
   # See how all your routes lay out with "rake routes"
 
+  match "tweet/:id" => "tweets#show"
+  
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
