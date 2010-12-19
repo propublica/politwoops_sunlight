b25e44dbc8c1967bdba5a313d4b2547290bd8b74
Breyten Ernsting <bje@dds.nl>
2010-12-19 20:43:41 +0100

    Add default scopes.

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,3 +1,5 @@
 class Politician < ActiveRecord::Base
   has_many :tweets
+  
+  default_scope :order => 'user_name'
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,8 @@
 class Tweet < ActiveRecord::Base
   belongs_to :politician
 
+  default_scope :order => 'modified DESC'
+  
   def details
     JSON.parse(tweet)
   end  
