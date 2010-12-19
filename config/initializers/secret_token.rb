e8ea7539ca11b89fb42c0e96f16211c2777b1837
Breyten Ernsting <bje@dds.nl>
2010-12-19 16:33:41 +0100

    Initial import of politwoops.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Politwoops::Application.config.secret_token = '87a8998cb9216aac0e255bb7a8a622cefbb43452cdd952f51efa5da59698ac80c3ae3e7e7e11e94df7cde2b2bd58a79c6a81d3e8b7f393ed8ff4037212103d81'
