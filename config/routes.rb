d7535edd1679d643544bc63e331871a15e2342c8
Breyten Ernsting <bje@dds.nl>
2011-07-27 19:22:53 +0200

    Fix paging on frontpage.

--- config/routes.rb
+++ config/routes.rb
@@ -49,7 +49,9 @@ Politwoops::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  
+
+  root :to => "tweets#index"
+    
   match "feed/" => "tweets#index", :format => :xml
   
   match "statistics/" => "statistics#index"
@@ -89,8 +91,6 @@ Politwoops::Application.routes.draw do
   resources :pages
   resources :groups
 
-  root :to => "tweets#index"
-    
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
