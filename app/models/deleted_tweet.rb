af2d679f826e8933cfcf4faddf59295a03604af9
Breyten Ernsting <bje@dds.nl>
2011-12-17 13:10:22 +0100

    Use a different table for deleted tweets, but the same model.

--- app/models/deleted_tweet.rb
+++ app/models/deleted_tweet.rb
@@ -1,24 +1,3 @@
-class DeletedTweet < ActiveRecord::Base
-  belongs_to :politician
-
-  default_scope :order => 'modified DESC'
-  
-  cattr_reader :per_page
-  @@per_page = 10
-  
-  def details
-    JSON.parse(tweet)
-  end  
-
-  def format
-    {
-      :created_at => created,
-      :updated_at => modified,
-      :id => id.to_s,
-      :politician_id => politician_id,
-      :details => details,
-      :content => content,
-      :user_name => user_name
-    }
-  end
+class DeletedTweet < Tweet
+  set_table_name "deleted_tweets"
 end
