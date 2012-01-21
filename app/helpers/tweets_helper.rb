d17cd8eb94f8257f5289db7af88b03a4eca9ea2a
Breyten Ernsting <bje@dds.nl>
2012-01-21 13:55:00 +0100

    Better way of auto linking usernames and hashtags.

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,50 +1,13 @@
 module TweetsHelper
+  def format_user_name(tweet_content)
+    tweet_content.gsub(/(@(\w+))/, %Q{<a href="http://twitter.com/\\2" target="_blank">\\1</a>})
+  end
+  
+  def format_hashtag(tweet_content)
+    tweet_content.gsub(/(#(\w+))/, %Q{<a href="https://twitter.com/#!/search?q=%23\\2" target="_blank">\\1</a>})
+  end
+  
   def format_tweet(tweet)
-    # return tweet.content
-    
-    details = tweet.details
-    return auto_link(tweet.content) unless details.has_key?('entities')
-    return auto_link(tweet.content) if details['entities'].empty?
-
-    content = tweet.content
-    
-    entities = []
-    entity_types = ['urls', 'user_mentions', 'hashtags']
-    entity_types.each do |entity_type|
-      next if not details['entities'].has_key?(entity_type)
-      
-      details['entities'][entity_type].each do |entity_info|
-        entity = {
-          :entity_start => entity_info['indices'][0],
-          :entity_stop => entity_info['indices'][1] 
-        }
-        if entity_type == 'urls'
-          entity[:source_text] = entity_info['url']
-          entity[:display_text] = entity_info['display_url'].nil? ? entity_info['url'].sub('http://', '') : entity_info['display_url']
-          entity[:url] = entity_info['expanded_url'].nil? ? entity_info['url'] : entity_info['expanded_url']
-        elsif entity_type == 'user_mentions'
-          entity[:source_text] = '@%s' % entity_info['screen_name']
-          entity[:display_text] = entity[:source_text]
-          entity[:url] = 'http://twitter.com/%s' % entity_info['screen_name']
-        else
-          entity[:source_text] = '#%s' % entity_info['text']
-          entity[:display_text] = '#%s' % entity_info['text']
-          entity[:url] = 'https://twitter.com/search/%%23%s' % entity_info['text']
-        end
-        entity[:result] = link_to entity[:display_text], entity[:url], :target => '_new'
-        entities << entity
-      end
-    end
-    
-    offset = 0
-    entities.sort_by {|e| e[:entity_start]}.each do |entity|
-      old_length = entity[:entity_stop] - entity[:entity_start]
-      content.slice!(entity[:entity_start] + offset, old_length)
-      content = content.insert(entity[:entity_start] + offset, entity[:result])
-      new_length = entity[:result].length
-      offset += (new_length - old_length)
-    end
-        
-    return content
+    content = auto_link(format_user_name(format_hashtag(tweet.content)), :html => { :target => '_blank' })
   end
 end
