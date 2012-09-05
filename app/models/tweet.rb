9ec0ec3336f7127203303978ec3ab21b4f3daf24
Eric Mill <konklone@gmail.com>
2012-09-05 13:24:17 -0400

    Removed typo game

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,13 +7,12 @@ class Tweet < ActiveRecord::Base
   # scope :latest, :order => 'created DESC'
   scope :deleted, :conditions => "deleted = 1 AND content IS NOT NULL"
   scope :with_content, :conditions => "content IS NOT NULL"
-  scope :typo_unchecked, :conditions => "typo_checked IS NULL"
   
   cattr_reader :per_page
   @@per_page = 10
   
   def self.random
-      reorder("RAND()").find(:first, :limit => 1)
+    reorder("RAND()").find(:first, :limit => 1)
   end
     
   def details
