b2292652559b66766b1a0d27a4275b6c779fcdd3
Caitlin <cweber@sunlightfoundation.com>
2012-09-17 11:30:53 -0400

    Copied code from tweets controller to the politicians controller. Will clean up soon.

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -9,6 +9,36 @@ class PoliticiansController < ApplicationController
     
     @tweets = DeletedTweet.includes(:politician => [:party]).where(:approved => true, :politician_id => @politician.id).paginate(:page => params[:page], :per_page => Tweet.per_page)
 
+    @states_list = Politician.all(:select => "DISTINCT(state)")
+    @states = []
+    @states_list.each do |sl|
+      if sl.state != nil 
+        @states.push(sl.state)
+      end
+    end
+    @states = @states.sort
+
+    @parties = Party.all
+    @offices = Office.all
+
+    @politicians = Politician.active
+
+    #check for filters
+    @filters = {'state' => nil, 'party' => nil, 'office' => nil  }
+    if params.has_key?(:state) && params[:state] != ""
+        @politicians = @politicians.where(:state => params[:state])
+        @filters['state'] = params[:state]
+    end
+    if params.has_key?(:party) && params[:party] != ""
+        party = Party.where(:name => params[:party])[0]
+        @politicians = @politicians.where(:party_id => party)
+        @filters['party'] = party.name
+    end
+    if params.has_key?(:office) && params[:office] != ""
+        @politicians = @politicians.where(:office_id => params[:office])
+        @filters['office'] = params[:office]
+    end
+
     respond_to do |format|
       format.html { render } # politicians/show
       format.rss  do
