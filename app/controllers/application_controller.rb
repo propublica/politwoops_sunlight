77c311961844e3a7aee0b3eb8edbcde2a95470ca
Drew Vogel <dvogel@sunlightfoundation.com>
2012-09-18 17:40:44 -0400

    Refactored the filter form into a shared template and moved the code into a shared method in the application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,33 @@ class ApplicationController < ActionController::Base
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
+
+  def enable_filter_form
+    @states = Politician.where("state IS NOT NULL").all(:select => "DISTINCT(state)").map do |row|
+      row.state
+    end
+    @states = @states.sort
+
+    @parties = Party.all
+    @offices = Office.all
+
+    @politicians = Politician.active
+
+    #check for filters
+    @filters = {'state' => nil, 'party' => nil, 'office' => nil  }
+    unless params.fetch('state', '').empty?
+        @politicians = @politicians.where(:state => params[:state])
+        @filters['state'] = params[:state]
+    end
+    unless params.fetch('party', '').empty?
+        party = Party.where(:name => params[:party])[0]
+        @politicians = @politicians.where(:party_id => party)
+        @filters['party'] = party.name
+    end
+    unless params.fetch('office', '').empty?
+        @politicians = @politicians.where(:office_id => params[:office])
+        @filters['office'] = params[:office]
+    end
+  end
+
 end
