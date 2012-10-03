dbec4753356c15bbc0eaba1fc8a9c9e804cb6f74
Politwoops staging <politwoops@politwoops.sunlightfoundation.com>
2012-10-03 21:01:04 +0000

    changed sorting on office and party models. Fixed gigya and display names on politician

--- app/models/office.rb
+++ app/models/office.rb
@@ -1,3 +1,4 @@
 class Office < ActiveRecord::Base
   has_many :politicians
+  default_scope :order => 'title ASC'
 end
--- app/models/party.rb
+++ app/models/party.rb
@@ -1,3 +1,4 @@
 class Party < ActiveRecord::Base
   has_many :politicians
+  default_scope :order => 'display_name ASC'
 end
