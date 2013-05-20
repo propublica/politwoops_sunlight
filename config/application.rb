424e1f005ede40d6eae4ba28248cdd89e6d78e3a
Ahmed Medhat Othman <amedhat3@gmail.com>
2013-05-20 22:16:41 +0200

    debuging the time zone

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,10 @@ module Politwoops
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    
     config.time_zone = 'Africa/Cairo'
+    #config.active_record.default_timezone = 'Africa/Cairo'
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
