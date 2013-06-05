334f58a7a177ecfbbe92f88f4e6785025f9f8901
Ahmed Medhat Othman <amedhat3@gmail.com>
2013-06-05 14:05:32 +0200

    add sending mail feature to the production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,5 +48,24 @@ Politwoops::Application.configure do
   config.active_support.deprecation = :notify
 
   config.site_url = "http://2ad.kelmetak.com"
+
+  config.action_mailer.raise_delivery_errors = true
+
+  # set delivery method to :smtp, :sendmail or :test
+  #config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
+
+  # these options are only needed if you choose smtp delivery
+  """
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.example.com',
+    :port           => 25,
+    :domain         => 'beta.2ad.kelmetak.com',
+    :authentication => :login,
+    :user_name      => 'www',
+    :password       => 'secret'
+  }
+  """
+
   
 end
