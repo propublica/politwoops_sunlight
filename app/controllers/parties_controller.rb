ed2a85615c8b01cdf01224620db7ff3f9656dea2
Drew Vogel <dvogel@sunlightfoundation.com>
2013-06-05 15:48:18 -0400

    Fixed parties page.

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,6 +1,13 @@
 class PartiesController < ApplicationController
 
+  include ApplicationHelper
+  before_filter :enable_filter_form
+
   def show    
+    @per_page_options = [20, 50]
+    @per_page = closest_value((params.fetch :per_page, 0).to_i, @per_page_options)
+    @page = [params[:page].to_i, 1].max
+
     @politicians = Party.where(:name => params[:name]).first.politicians.active.map {|politician| politician.id}
     @tweets = DeletedTweet.includes(:politician => [:party]).where(:politician_id => @politicians, :approved => true).paginate(:page => params[:page], :per_page => Tweet.per_page)
     render "tweets/index"
