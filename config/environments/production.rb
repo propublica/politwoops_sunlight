434bf6f337178e6dbe27b4445e864daff94d35cb
Drew Vogel <dvogel@sunlightfoundation.com>
2013-11-18 11:51:08 -0500

    public-read for avatar images on s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Politwoops::Application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_permissions => :public_read,
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
