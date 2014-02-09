9983b82f5ffb85cfdce211191a34583c3fda3465
Mohamed Al-Khazendar <mohamed.khazendar@nezal.com>
2014-02-09 15:40:04 +0200

    fix for hamming distance, for different size strings

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -167,10 +167,11 @@ class Admin::TweetsController < Admin::AdminController
 
   def is_similar(str1, str2)
     
-    hamming_distance = hamming_distance(str1,str2)
+    max_size = [str1.size,str2.size].max
+    min_size = [str1.size,str2.size].min
+    hamming_distance = hamming_distance(str1,str2) + max_size -  min_size
     max_hamming = configuration[:max_auto_reject_hamming].to_i
     lcs_size = lcs(str1, str2).size
-    max_size = [str1.size,str2.size].max
     max_lcs_ratio = configuration[:max_lcs_ratio].to_f
     if (hamming_distance <= max_hamming || lcs_size.to_f/max_size.to_f >= max_lcs_ratio )
       return true
