0c4cad35cde27b09f2723a1ad7f84d73a0d1bbba
Jaime Andres <andresdavila6@gmail.com>
2014-02-28 17:39:27 -0200

    [#66516906] implements application_layout html and styles

--- app/assets/javascripts/politwoops.js
+++
@@ -1,9 +0,0 @@
-(function($){
-    $(function(){
-        $('img.politician-avatar').error(function(e){
-            e.preventDefault();
-            $(this).attr('src', '/assets/avatar_missing_male.png');
-        });
-    });
-})(jQuery);
-
