64cc0e46887d3645e9b827123df2c749f0f0b00e
Drew Vogel <dvogel@sunlightfoundation.com>
2014-02-28 14:09:16 -0500

    Properly return 404 status codes.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,10 +3,12 @@ class ErrorsController < ApplicationController
   include ApplicationHelper
   
   def not_found
-    render "errors/not_found"
+    render "errors/not_found", :status => 404
   end
 
   def down
+    # We don't generate a 500 here because this is simply for regenerating a
+    # static error page.
     render "errors/status_5xx"
   end
 end
