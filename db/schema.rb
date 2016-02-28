3070940bef7df350ac341e45ce17b26e8f782191
Clayton Dunwell <clayton.dunwell@gmail.com>
2016-02-28 12:49:46 -0500

    resolve inconsistency on production
    don't try this at home

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140612143804) do
+ActiveRecord::Schema.define(:version => 20160125211055) do
 
   create_table "account_links", :force => true do |t|
     t.integer  "politician_id"
