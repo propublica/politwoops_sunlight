ba3fb68689e0ff18a65ee4d4f50dd5d2f8dcc65b
Derek Willis <derek.willis@propublica.org>
2016-12-07 17:35:13 -0500

    added asset initializer

---
+++ config/initializers/assets.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w(propublica_base/base.css propublica_base/master.css propublica_base/print.css propublica_base/woland.css)
