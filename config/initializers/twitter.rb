d3d056f59cc4cd74fb646b934bcdd8e3c28812f6
Derek Willis <derek.willis@propublica.org>
2016-12-07 17:35:27 -0500

    updated twitter initializer

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -2,6 +2,6 @@ cfg = YAML.load_file "#{Rails.root}/config/config.yml"
 $twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = cfg[:twitter][:consumer_key]
   config.consumer_secret = cfg[:twitter][:consumer_secret]
-  config.oauth_token = cfg[:twitter][:oauth_token]
-  config.oauth_token_secret = cfg[:twitter][:oauth_token_secret]
+  config.access_token = cfg[:twitter][:oauth_token]
+  config.access_token_secret = cfg[:twitter][:oauth_token_secret]
 end
