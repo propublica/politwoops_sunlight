152ebb5513161f80aeae8f4716519da9f1e501c3
Derek Willis <derek.willis@propublica.org>
2016-12-09 14:39:41 -0500

    prod static files config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Politwoops::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.log_level = :info
+
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
@@ -30,7 +32,7 @@ Politwoops::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
